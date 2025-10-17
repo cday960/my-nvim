dab4938b628e0ffa42f0acc3b225d80544d0ecf8
cday960 <c.day960@gmail.com>
2025-10-17 10:41:45 -0500

    ruff formatting for conform

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,6 +1,7 @@
 require('conform').setup({
 	formatters_by_ft = {
-		python = { "black" },
+		-- python = { "black" },
+		python = { "ruff_format", "ruff_fix" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
